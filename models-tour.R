249a3bc4add6b6041644d55eb9d882a835d1def2
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2023-12-09 11:20:04 +1100

    extended model comparison

--- models-tour.R
+++ models-tour.R
@@ -39,10 +39,18 @@ record <- animate(m1_a[,c(2,3,4,5,7)],
                                     ncol=2, byrow=FALSE),
                      rescale=TRUE)
 
-# !!
-# I had to modify the animate() function
-# to keep record of consecutive frames
-# added line 80: record[[i]] <- list(i = i, step = step)
+path <- save_history(m1_a[,c(2,3,4,5,7)],
+             dependence_tour(c(1,1,1,1,2)),
+             start = matrix(c(1,0,0,0,0,0,0,0,0,1),
+                            ncol=2, byrow=FALSE),
+             max = 50,
+             rescale=TRUE)
+
+animate_xy(m1_a[,c(2,3,4,5,7)],
+        planned_tour(path),
+        axes="bottomleft",
+        rescale=TRUE)
+
 
 # HERE: new script starts
 # diagnostic plots
@@ -51,7 +59,9 @@ record <- animate(m1_a[,c(2,3,4,5,7)],
 library(tidyr)
 library(patchwork)
 
-data <- as.matrix(m1_a[,c(2,3,4,5,7)])
+data1 <- as.matrix(m1_a[,c(2,3,4,5,7)])
+data2 <- as.matrix(m2_a[,c(2,3,4,5,7)])
+data12 <- as.matrix(m_12[,c(2,3,4,5,14)])
 
 # supplementary functions
 
@@ -64,67 +74,93 @@ get_p_value_chisq <- function(ndata, k = 5) {
 }
 
 # get pvalues for independence test for the tour
-get_pvalues <- function(data, record) {
-  chisq_pval <- sapply(record, function(x) get_p_value_chisq(data %*% x$step$proj))
-  data.frame(step = seq_along(record),
+get_pvalues <- function(data, path) {
+  chisq_pval <- apply(path, 3, function(x) get_p_value_chisq(data %*% x))
+  data.frame(step = 1:dim(path)[3],
              log10_pval = -log10(chisq_pval))
 }
 
 # extract projections recorded during the tour
-get_projections <- function(record) {
-  projections <- t(sapply(record,
-                          function(x) {
-                            proj <- x$step$proj
-                            proj[-nrow(proj), 1] # first column without last row
-                          }))
+get_projections <- function(path) {
+  attr(path, "data") <- NULL
+  projections <- t(sapply(1:dim(path)[3],
+                          function(x)
+                            c(path[-nrow(path),1,x])
+  ))
 
   projections_df <- as.data.frame(projections)
   projections_df$step <- 1:nrow(projections_df)
   pivot_longer(projections_df, -step)
 }
 
-# calculate projections and pvalues
-projections_long <- get_projections(record)
-chisq_pval_log <- get_pvalues(data, record)
-# where is max dependence
-p_max <- which.max(chisq_pval_log$log10_pval)
-ndata <- data.frame(data %*% record[[p_max]]$step$proj)
+projections_long <- get_projections(path)
 
-# now plots
-pl_coef <- ggplot(projections_long, aes(step, value, color=name)) +
-  geom_line() +
-  theme_bw() +
-  ggtitle("projection weights") +
-  geom_vline(xintercept = p_max, color="red", lty=2) +
-  theme(legend.position = "none")
+get_triplots <- function(data, path, datacolor = "orange", modelname = "") {
+  chisq_pval_log <- get_pvalues(data, path)
+  # where is max dependence
+  p_max <- which.max(chisq_pval_log$log10_pval)
+  ndata <- data.frame(data %*% matrix(path[,,p_max], ncol = 2))
+  # very very ugly solution
+  # do not look at it
+  ndata1 <- data.frame(data1 %*% matrix(path[,,p_max], ncol = 2))
+  ndata2 <- data.frame(data2 %*% matrix(path[,,p_max], ncol = 2))
+  ndata12 <- data.frame(data12 %*% matrix(path[,,p_max], ncol = 2))
 
+  # now plots
+  pl_coef <- ggplot(projections_long, aes(step, value, color=name)) +
+    geom_line() +
+    theme_bw() +
+    ggtitle("projection weights") +
+    geom_vline(xintercept = p_max+0.5, color="red", lty=2) +
+    theme(legend.position = "none")
 
-pl_pval <- ggplot(chisq_pval_log, aes(step, log10_pval)) +
-  geom_point() +
-  geom_line() +
-  theme_bw() +
-  geom_vline(xintercept = p_max, color="red", lty=2) +
-  ggtitle("log10 p-values for chisq independence test", "large values suggest structure")
+  pl_pval <- ggplot(chisq_pval_log, aes(step, log10_pval)) +
+    geom_point(color=datacolor) +
+    geom_step(color=datacolor) +
+    theme_bw() +
+    geom_vline(xintercept = p_max+0.5, color="red", lty=2) +
+    ggtitle("log10 p-values for chisq independence test", "large values suggest structure")
 
-pl_resid <- ggplot(ndata, aes(X1, X2)) +
-  geom_point() +
-  geom_smooth(se = FALSE, color = "orange", linewidth=2) +
-  theme_bw() +
-  xlab("projection") +
-  ylab("residuals") +
-  ggtitle("residual vs. projection plot", paste0("for step ", p_max))
+  pl_resid <- ggplot(ndata, aes(X1, X2)) +
+# very very ugly solution
+# do not look at it
+    geom_point(data=ndata1, color="black", size=0.1, alpha-0.2) +
+    geom_point(data=ndata2, color="orange", size=0.1, alpha-0.2) +
+    geom_point(data=ndata12, color="blue", size=0.1, alpha-0.2) +
+    geom_point(color="grey", size=0.5, alpha-0.2) +
 
-pl_proj <- ggplot(data = data.frame(var = 1:4, coef = record[[p_max]]$step$proj[1:4,1]),
+    geom_smooth(data=ndata1, se = FALSE, color = "black", linewidth=1) +
+    geom_smooth(data=ndata2, se = FALSE, color = "orange", linewidth=1) +
+    geom_smooth(data=ndata12, se = FALSE, color = "blue", linewidth=1) +
+
+    geom_smooth(se = FALSE, color = datacolor, linewidth=2) +
+    theme_bw() +
+    xlab("projection") +
+    ylab("residuals") +
+    ggtitle(paste0(modelname, " vs. projection plot"), paste0("for step ", p_max))
+
+  pl_proj <- ggplot(data = data.frame(var = 1:4, coef =c(path[1:4,1,p_max])),
                     aes(var, coef, fill = factor(var))) +
-                   geom_col() +
-  theme_bw() +
-  theme(legend.position = "none") +
-  coord_flip()
+    geom_col() +
+    theme_bw() +
+    theme(legend.position = "none") +
+    coord_flip()
 
+  list(pl_coef = pl_coef,
+        pl_pval = pl_pval,
+        pl_resid = pl_resid,
+        pl_proj = pl_proj)
+}
 
-(pl_pval + pl_resid) /
-  (pl_coef + pl_proj) +
-  plot_layout(heights = c(3,1))
+# calculate actual plots
+plots1 <- get_triplots(data1, path, datacolor = "black", modelname = "lm residuals")
+plots2 <- get_triplots(data2, path, datacolor = "orange", modelname = "rpart residuals")
+plots12 <- get_triplots(data12, path, datacolor = "blue", modelname = "diff residuals")
+
+(plots1$pl_resid + plots2$pl_resid + plots12$pl_resid) /
+  (plots1$pl_proj + plots2$pl_proj + plots12$pl_proj) /
+(plots1$pl_pval + plots2$pl_pval + plots12$pl_pval) +
+  plot_layout(heights = c(3,1,1))
 
 
 
@@ -133,7 +169,7 @@ pl_proj <- ggplot(data = data.frame(var = 1:4, coef = record[[p_max]]$step$proj[
 
 
 
-record <- animate_xy(m1_a[,c(2,3,4,5,7)],
+animate_xy(m1_a[,c(2,3,4,5,7)],
            dependence_tour(c(1,1,1,1,2)),
            axes="bottomleft",
            start = matrix(c(1,0,0,0,0,0,0,0,0,1),
